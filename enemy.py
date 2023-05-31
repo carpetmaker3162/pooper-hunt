804278984e17dbe4a67eabfbeaf410cb60bb166f
can pooper <domesticatedhuman9999@gmail.com>
2023-05-31 19:50:06 -0400

    improve death aniamtion

--- enemy.py
+++ enemy.py
@@ -39,12 +39,12 @@ class Enemy(Entity):
         self.y_speed = y_speed
         self.default_x_speed = x_speed
         self.default_y_speed = y_speed
-        self.panic_multiplier = 1
+        self.panic_multiplier = 2 # amount speed is increased by when shot at
 
         # enemy state variables
         self.goal = None
         self.mode = "wander"
-        self.scheduled = []
+        self.scheduled = [] # scheduled actions, for more complicated movement like peeking
         self.recovery_time = float("inf") # time at which enemy stops hiding
         self.comfort_hp = self.max_hp # enemy panics if below comfort hp
         self.dead = False
@@ -55,21 +55,27 @@ class Enemy(Entity):
         # sort scheduled actions by start times
         self.scheduled.sort(key=lambda a: a.start)
 
+        # iterate through actions
         for i, action in enumerate(self.scheduled):
+            # delete action if it has finished completely
             if action.start + action.duration < current_time:
                 self.scheduled.remove(action)
+            # execute the first action that is in process
             elif action.start <= current_time <= action.start + action.duration:
                 self.x_speed = action.x_speed
                 self.y_speed = action.y_speed
+                # apply y-acceleration (and modify action's base y-speed as well)
                 self.y_speed += action.y_acceleration
                 self.scheduled[i].y_speed = self.y_speed
                 break
 
-        # move and other
+        # move
         super().update()
         
-        # play death animation if enemy dies
-        if self.hp <= 0:
+        # play death animation if enemy dies. do not attempt to schedule if already 
+        # dead, because then image will be changed to right_dead regardless of 
+        # direction since x-speed is set to 0 by the death animation
+        if self.hp <= 0 and not self.dead:
             self.dead = True
             self.death_animation()
 
@@ -78,7 +84,7 @@ class Enemy(Entity):
             self.panic(crates)
             self.comfort_hp = self.hp
 
-        # stop if enemy is now hiding behind a crate
+        # stop moving if enemy is now hiding behind a crate completely
         if self.mode == "panic":
             for crate in crates:
                 if crate.encloses(self):
@@ -88,20 +94,25 @@ class Enemy(Entity):
                     self.recovery_time = current_time + random.randint(8000, 18000)
 
         if self.mode == "hide":
+            # continue wandering and clear scheduled actions if recovered
+            # (basically reset to default behaviour)
             if current_time >= self.recovery_time:
                 self.mode = "wander"
                 self.scheduled = []
                 self.x_speed = self.default_x_speed
                 self.y_speed = self.default_y_speed
             else:
+                # schedule next time enemy peeks from behind crate
                 if not self.scheduled:
                     next_peek = random.randint(1000, 5000)
                     self.peek(start = current_time + next_peek, duration=1000, dx=100, dy=0)
-                # stop when enemy is back behind crate
+                    
+                # stop when enemy is back behind crate (badly written code)
                 if any(crate.encloses(self) for crate in crates):
                     self.x_speed = 0
                     self.y_speed = 0
         
+        # change image based on enemy direction
         if self.x_speed < 0 and not self.dead:
             self.image = get_image(IMAGES["left"], 50, 50)
         elif self.x_speed >= 0 and not self.dead:
@@ -114,18 +125,27 @@ class Enemy(Entity):
     def change_dir(self):
         self.x_speed *= -1
     
+    # play death animation (does not kill entity)
     def death_animation(self):
-        current_time = pygame.time.get_ticks()
+        # override currently scheduled actions in case enemy is in the middle of peeking
+        self.scheduled = []
 
+        # change image to dead image
         if self.x_speed < 0:
             self.image = get_image(IMAGES["left_dead"], 50, 50)
         elif self.x_speed >= 0:
             self.image = get_image(IMAGES["right_dead"], 50, 50)
 
-        self.schedule_action(Action(0, -1000, current_time, 10000, 0.5))
+        # make enemy fall (with y-acceleration to imitate gravity)
+        current_time = pygame.time.get_ticks()
+        self.schedule_action(Action(0, -800, current_time, 10000, 0.8))
 
+    # peek out from behind a crate
     def peek(self, start, duration, dx, dy):
+        # duration is a there-and-back trip so divide it in half
         duration //= 2
+
+        # schedule actions for there-and-back
         self.schedule_action(Action(dx, dy, start, duration))
         self.schedule_action(Action(-dx, -dy, start + duration, duration))
 
